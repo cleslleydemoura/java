349f0c276e39c2e8201ba411485b855867f24d88
Cleslley de Moura <100368699+cleslleydemoura@users.noreply.github.com>
2024-01-29 04:15:43 -0300

    absolute first java project

---
+++ javando/absolutefirstjavaproject/src/absolutefirstjavaproject/absolutefirstjavaproject.java
@@ -0,0 +1,9 @@
+package absolutefirstjavaproject;
+
+public class absolutefirstjavaproject {
+	/* method main é responsável por executar a classe, pegando o conteúdo que está
+	 * dentro do method main e executar.*/
+	public static void main(String[] args) {
+		System.out.print("Hello World! This is my first JAVA project!");
+	}
+}
