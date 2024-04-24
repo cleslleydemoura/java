d13cdf773ef996bd643904ebdad3cc21effb5505
Cleslley de Moura <100368699+cleslleydemoura@users.noreply.github.com>
2024-04-24 10:27:36 -0300

    atividades de java em aula

---
+++ java-aulas-atividades/repeticao/src/repeticao_java/Repeticao_ex.java
@@ -0,0 +1,10 @@
+package repeticao_java;
+
+public class Repeticao_ex {
+	public static void main(String[] args) {
+		for(int i=0;i<=10;i++){
+	        System.out.println(i + ") printf");
+	    }
+	}
+}
+
